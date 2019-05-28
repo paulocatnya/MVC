d94b5ea61e941311a4e096f3a406b809bffd57ea
Paulo Roberto <Paulo Roberto>
2019-05-28 11:54:48 -0300

    Seller Service basic FindAll

--- SalesWebMvc/Data/SeedingService.cs
+++ SalesWebMvc/Data/SeedingService.cs
@@ -29,14 +29,14 @@ namespace SalesWebMvc.Data
             Department d4 = new Department(4, "Books");
 
             Seller s1 = new Seller(1,"Paulo","prpaulo@",new DateTime(1998,4,21),1900,d1);
-            Seller s2 = new Seller(2,"Roberto","paulocatnya@",new DateTime(1998,4,21),1900,d2);
-            Seller s3 = new Seller(3,"Caetano","prcr@",new DateTime(1998,4,21),1900,d3);
-
+            Seller s2 = new Seller(2,"Roberto","paulocatnya@",new DateTime(1998,4,21),2100,d2);
+            Seller s3 = new Seller(3,"Caetano","prcr@",new DateTime(1998,4,21),9000,d3);
+            Seller s4 = new Seller(4, "Rodrigues", "op3prcr@", new DateTime(1998, 4, 21), 9000, d3);
 
             SalesRecord sr1 = new SalesRecord(1, new DateTime(2019, 4, 15), 500, SaleStatus.PENDING, s1);
             SalesRecord sr2 = new SalesRecord(2, new DateTime(2019, 4, 15), 1500, SaleStatus.BILLED, s2);
             SalesRecord sr3 = new SalesRecord(3, new DateTime(2019, 1, 15), 3500, SaleStatus.CANCELED, s3);
-            SalesRecord sr4 = new SalesRecord(4, new DateTime(2018, 4, 15), 2500, SaleStatus.PENDING, s2);
+            SalesRecord sr4 = new SalesRecord(4, new DateTime(2018, 4, 15), 2500, SaleStatus.PENDING, s4);
 
             _context.Department.AddRange(d1,d2,d3,d4);
             _context.Seller.AddRange(s1,s2,s3);
