f7e63f277304db6ad3eb90867c88e24a61674774
Paulo Roberto <Paulo Roberto>
2019-06-07 12:28:39 -0300

    Grouping Search implementing

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -20,11 +20,11 @@ namespace SalesWebMvc.Models
             
         }
 
-        public void addSeller(Seller seller)
+        public void AddSeller(Seller seller)
         {
             Sellers.Add(seller);
         }
-        public double totalSales(DateTime initial, DateTime final)
+        public double TotalSales(DateTime initial, DateTime final)
         {
             return Sellers.Sum(seller => seller.TotalSales(initial,final));
         }
--- SalesWebMvc/Models/SalesRecord.cs
+++ SalesWebMvc/Models/SalesRecord.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.ComponentModel.DataAnnotations;
 using SalesWebMvc.Models.Enums;
 
 namespace SalesWebMvc.Models
@@ -7,9 +7,17 @@ namespace SalesWebMvc.Models
     public class SalesRecord
     {
         public int Id { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}") ]
         public DateTime Date { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double Ammount { get; set; }
+
+
         public SaleStatus Status { get; set; }
+
+
         public Seller Seller { get; set; }
 
         public SalesRecord()
